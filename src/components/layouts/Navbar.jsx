ac09fbaa04076e2a102b3f19376546f6984f322e
rekujin <152603345+rekujin@users.noreply.github.com>
2025-06-12 20:12:22 +0700

    Update Navbar.jsx

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -9,7 +9,7 @@ import { useSearch } from "../../context/SearchContext";
 import { useMobile } from "../../hooks/useMobile";
 
 // Components
-import MobileBottomNavbar from "./mobileBottomNavbar";
+import MobileBottomNavbar from "./MobileBottomNavbar";
 import CreateModal from "../modals/CreateModal";
 
 // Styles
